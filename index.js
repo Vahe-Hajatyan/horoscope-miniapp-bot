030170c456dd838b268ec9b293ea7921d880419e
no-name3 <hajatyanvahe@gmail.com>
2024-08-19 01:11:01 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '7430822939:AAGEJEAUk1By-kK2TW1Xpn_jTciNIsvyO9Q';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  if (text === '/start') {
+    await bot.sendMessage(chatId, 'open web site', {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'open web site',
+              web_app: { url: 'https://horoscope-miniapp-upp1.vercel.app' },
+            },
+          ],
+        ],
+      },
+    });
+  }
+});
